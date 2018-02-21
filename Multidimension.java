713864447b692a013831bd25ac460a2eb111cc28
Jessy Benitez <Jessybntz@gmail.com>
2018-02-21 12:27:08 -0500

    First commit

---
+++ Multidimension.java
@@ -0,0 +1,36 @@
+public class Multidimension {
+	public static void main(String [] args) {
+		String [][] roster = new String [6][5];
+		roster[4][4] = "nika";
+
+		String [][] yellowPage = {{"shaheer","571-365-4566"},
+				{"shahid","703-224-5564"},
+				{"patrick","571-256-4456"}};
+		String [][][] threeD = new String[2][2][2];
+
+		String [][][] yellowPages = {{{"shaheer","571-365-4566"},
+			{"shahid","703-224-5564"},
+			{"patrick","571-256-4456"}},
+
+				{{"shah","571-365-4566"},
+				{"hid","703-224-5564"},
+				{"pat","571-256-4456"}}
+		};
+
+		for(int row=0; row<yellowPage.length; row++) {
+			for(int col=0; col<yellowPage[row].length; col++) {
+				System.out.println(yellowPage[row][col]);
+
+			}
+		}
+		for(int row=0; row<yellowPages.length; row++) {
+			for(int col=0; col<yellowPages[row].length; col++) {
+				for (int j=0; j<yellowPages.length; j++) {
+					System.out.println(yellowPages[row][col][j]);
+
+				}
+			}
+		}
+	}
+}
+
